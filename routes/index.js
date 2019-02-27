cedf6a1db7157b24ac922b95937ca906b3d7dd8f
Joey Deras <jmderas@yahoo.com>
2019-02-27 11:38:57 -0500

    report-routes-added-fullCrud

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send("hello world");
-});
+router.get('/', function(req, res) {
+    res.send('homepage')
+})
+
+router.use("/reports", require("./reports.js"))
 
 module.exports = router;

93c4a3cb651fd93ce4c5149f666b5445f0df212a
Joey Deras <jmderas@yahoo.com>
2019-02-27 10:06:33 -0500

    routes-hello-world

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send("hello world");
-});
+app.set("view engine", "hbs");
+
+app.use(require('./routes/index'))
 
 app.listen(3000, () => console.log("listening on Port 3000"));

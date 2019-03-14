ff26332058c4d21640a4e43cdba7b529b4e23c50
Joey Deras <jmderas@yahoo.com>
2019-03-14 09:33:02 -0400

    added heroku deployment code

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/pothole");
+// mongoose.connect("mongodb://localhost/pothole")
 
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.DB_URL);
+} else {
+  mongoose.connect("mongodb://localhost/pothole");
+}
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const hbs = require("hbs");
 const methodOverride = require("method-override");
@@ -8,7 +8,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "hbs");
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
-app.use(require('./routes/index'))
+app.use(require("./routes/index"));
 hbs.registerPartials(__dirname + "/views/partials");
 
-app.listen(3050, () => console.log("listening on Port 3050"));
+app.set("port", process.env.PORT || 3050);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
+
+//app.listen(3050, () => console.log("listening on Port 3050"));
